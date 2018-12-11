02b422d51fd341cf19a6db0fbb6959a771ce72c4
John Mark Redding <johnmarkredding@gmail.com>
2018-12-11 19:58:00 +0000

    Automatically backed up by Learn

---
+++ models/pig_latinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+  def piglatinize(text)
+    parsed_words = text.split(" ").map do |word|
+      newword = ""
+      if word.match(/^[aeiouAEIOU]/)
+        newword = vowel_start(word)
+      elsif word.match(/^[bcdfghjklmnpqrstvwxyz]+/)
+        newword = consonant_start(word)
+      else
+        newword = word
+      end
+      newword
+    end
+    parsed_words.join(" ")
+  end
+  
+  def vowel_start(word)
+    "#{word[1..-1]}#{word[0]}way"
+  end
+  
+  def consonant_start(word)
+    prefix = word[/^[bcdfghjklmnpqrstvwxyz]+/]
+    "#{word[ending.size..-1]}#{prefix}ay"
+  end
+end
